3247556a0695cc496a4f48c539c3e1937bb8f322
phphint <124293060+phphint@users.noreply.github.com>
2024-01-05 01:57:47 -0500

    Update NavbarGeneral.tsx

--- nextjs-app/components/GeneralLayout/NavbarGeneral.tsx
+++ nextjs-app/components/GeneralLayout/NavbarGeneral.tsx
@@ -3,14 +3,15 @@ import NavMenu from '../NavMenu'; // Adjust the path according to your directory
 
 const Navbar = () => {
   return (
-    <section className="bg-[#35345c] text-white w-full nav-general">
+    <section className="bg-[#35345c] text-white w-full">
       <div className="container mx-auto px-6 py-3 flex justify-between items-center">
         <NavMenu />
 
-        {/* Button aligned to the right - hidden on mobile */}
-        <button className="hidden md:inline-block bg-[#504a7a] text-[#bfbfc7] py-2 px-4 rounded">
+        {/* Button aligned to the right */}
+        <button className="bg-[#504a7a] text-[#bfbfc7] py-2 px-4 rounded">
           Sign-up  
         </button>
+        
       </div>
     </section>
   );
